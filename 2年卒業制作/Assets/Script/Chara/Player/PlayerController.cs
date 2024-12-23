a713d85021a2dfb8ece5a415d0fb7977f7c31cbc
nkc20236093 <nkc23.taki.eita@gmail.com>
2024-12-23 14:24:52 +0900

    じゃんけんシステム作成途中、プレイヤー移動作成

--- 2年卒業制作/Assets/Script/Chara/Player/PlayerController.cs
+++ 2年卒業制作/Assets/Script/Chara/Player/PlayerController.cs
@@ -5,23 +5,126 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rigid;
+    Janken[] jankens = new Janken[3];
+    int select = 0;
+
+    public enum HandType
+    {
+        None,
+        Rock,
+        Paper,
+        Scissors
+    }
+
+    public class Janken
+    {
+        public HandType hand;
+        public Janken(HandType selecthand)
+        {
+            hand = selecthand;
+        }
+        public virtual void HandEffect() { }
+    }
+
+    public class Rock : Janken
+    {
+        public Rock() : base(HandType.Rock)
+        {
+
+        }
+        public override void HandEffect()
+        {
+            Debug.Log("グー");
+        }
+    }
+
+    public class Paper : Janken
+    {
+        public Paper() : base(HandType.Paper)
+        {
+
+        }
+        public override void HandEffect()
+        {
+            Debug.Log("パー");
+        }
+    }
+
+    public class Scissors : Janken
+    {
+        public Scissors() : base(HandType.Scissors)
+        {
+
+        }
+        public override void HandEffect()
+        {
+            Debug.Log("チョキ");
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         Camera.main.transform.localEulerAngles = Vector3.zero;
         rigid = GetComponent<Rigidbody>();
+
+        jankens[0] = new Rock();
+        jankens[1] = new Scissors();
+        jankens[2] = new Paper();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Action();
         Move();
     }
+
+    void Action()
+    {
+        // 仮にチョキをi、グーをu、パーをpとしたとき
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            select = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            select = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            select = 2;
+        }
+        // じゃんけん発動キーがjだと仮定して
+        if (Input.GetKeyDown(KeyCode.J)) 
+        {
+            jankens[select].HandEffect();
+        }
+    }
+
     void Move()
     {
-        Vector3 movedirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-        rigid.AddForce(0.5f * movedirection, ForceMode.Impulse);
         CameraCon();
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        if (moveDirection != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            rigid.AddForce(moveDir * 5f);
+        }
+
+        // プレイヤーの向きをカメラの向きに合わせる
+        Vector3 lookDirection = new Vector3(horizontalInput, 0, verticalInput);
+        if (lookDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), 3f * Time.deltaTime);
+        }
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
     }
     void CameraCon()
     {
