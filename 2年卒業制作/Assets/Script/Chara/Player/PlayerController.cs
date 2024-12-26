995dfdbe7fe13874f3e534339783cd0d3834cfd5
nkc20236093 <nkc23.taki.eita@gmail.com>
2024-12-26 15:45:23 +0900

    じゃんけんシステム作成中

--- 2年卒業制作/Assets/Script/Chara/Player/PlayerController.cs
+++ 2年卒業制作/Assets/Script/Chara/Player/PlayerController.cs
@@ -7,34 +7,94 @@ public class PlayerController : MonoBehaviour
     Rigidbody rigid;
     Janken[] jankens = new Janken[3];
     int select = 0;
-
+    AudioSource audioSource;
+    [SerializeField] GameObject[] Fingers;
+    LineRenderer[] lineRenderers = new LineRenderer[2];
 
     public class Janken
     {
-        public virtual void HandEffect() { }
+        public virtual void HandEffect(AudioSource audio, Rigidbody rigid, Transform transform, GameObject[] Fingers, LineRenderer[] lineRenderers) { }
     }
 
     public class Rock : Janken
     {
-        public override void HandEffect()
+        public override void HandEffect(AudioSource audio, Rigidbody rigid, Transform transform, GameObject[] Fingers, LineRenderer[] lineRenderers)
         {
             Debug.Log("グー");
+
+            lineRenderers[0].enabled = false;
+            lineRenderers[1].enabled = false;
+
+            // じゃんけん発動キーがjだと仮定して
+            if (Input.GetKey(KeyCode.J))
+            {
+
+            }
         }
     }
 
     public class Paper : Janken
     {
-        public override void HandEffect()
+        public override void HandEffect(AudioSource audio, Rigidbody rigid, Transform transform, GameObject[] Fingers, LineRenderer[] lineRenderers)
         {
             Debug.Log("パー");
+
+            lineRenderers[0].enabled = false;
+            lineRenderers[1].enabled = false;
+
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+            if (moveDirection != Vector3.zero)
+            {
+                float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+                Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+                rigid.AddForce(moveDir * 5f);
+            }
         }
     }
 
     public class Scissors : Janken
     {
-        public override void HandEffect()
+        public override void HandEffect(AudioSource audio, Rigidbody rigid, Transform transform, GameObject[] Fingers, LineRenderer[] lineRenderers)
         {
             Debug.Log("チョキ");
+            // じゃんけん発動キーがjだと仮定して
+            if (Input.GetKey(KeyCode.J))
+            {
+                for (int i = 0; i < Fingers.Length; i++)
+                {
+                    lineRenderers[i].enabled = true;
+
+                    float lazerDistance = 10f;
+                    Vector3 direction = Camera.main.transform.forward * lazerDistance;
+                    Vector3 pos = Fingers[i].transform.position;
+
+                    RaycastHit hit;
+                    Ray ray = new Ray(pos, Camera.main.transform.forward);
+                    lineRenderers[i].SetPosition(0, Fingers[i].transform.position);
+
+                    if (Physics.Raycast(ray, out hit, lazerDistance))
+                    {
+                        lineRenderers[i].SetPosition(1, pos + direction);
+                        Debug.Log("ヒット");
+
+                        if (hit.collider.gameObject.CompareTag("LightGimmick"))
+                        {
+                            Debug.Log("ヒット");
+                        }
+                    }
+
+                    Debug.DrawRay(ray.origin, ray.direction, Color.red, 0.1f);
+                }
+            }
+            else
+            {
+                lineRenderers[0].enabled = false;
+                lineRenderers[1].enabled = false;
+            }
         }
     }
 
@@ -43,19 +103,20 @@ public class PlayerController : MonoBehaviour
     {
         Camera.main.transform.localEulerAngles = Vector3.zero;
         rigid = GetComponent<Rigidbody>();
-
+        audioSource = GetComponent<AudioSource>();
         jankens[0] = new Rock();
         jankens[1] = new Scissors();
         jankens[2] = new Paper();
+        lineRenderers[0] = Fingers[0].GetComponent<LineRenderer>();
+        lineRenderers[1] = Fingers[1].GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        CameraCon();
         Action();
-        Move();
     }
-
     void Action()
     {
         // 仮にチョキをi、グーをu、パーをpとしたとき
@@ -71,37 +132,9 @@ public class PlayerController : MonoBehaviour
         {
             select = 2;
         }
-        // じゃんけん発動キーがjだと仮定して
-        if (Input.GetKeyDown(KeyCode.J)) 
-        {
-            jankens[select].HandEffect();
-        }
+        jankens[select].HandEffect(audioSource, rigid, transform, Fingers, lineRenderers);
     }
 
-    void Move()
-    {
-        CameraCon();
-
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
-
-        if (moveDirection != Vector3.zero)
-        {
-            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
-            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-            rigid.AddForce(moveDir * 5f);
-        }
-
-        // プレイヤーの向きをカメラの向きに合わせる
-        Vector3 lookDirection = new Vector3(horizontalInput, 0, verticalInput);
-        if (lookDirection != Vector3.zero)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), 3f * Time.deltaTime);
-        }
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
-    }
     void CameraCon()
     {
         if (Input.GetKeyDown(KeyCode.K))
@@ -122,6 +155,12 @@ public class PlayerController : MonoBehaviour
             direction.x = Mathf.Clamp(direction.x, -15, 45);
             direction.z = 0;
             Camera.main.transform.localEulerAngles = direction;
+
+            // プレイヤーの向きをカメラの向きに合わせる
+            Vector3 PlayerRotation = transform.eulerAngles;
+            PlayerRotation.y = Input.GetAxis("Mouse X");
+            transform.eulerAngles = new Vector3(0, PlayerRotation.y, 0);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(directionY), 3f * Time.deltaTime);
         }
     }
 }
