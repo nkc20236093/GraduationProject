b3a61fb50775ba883addf533af7667cf5a8a60db
nkc20236032 <nkc23.matsui.kaito@gmail.com>
2025-02-05 14:31:11 +0900

    ゲームオーバー演出

---
+++ 2年卒業制作/Assets/Script/Shader/TransitionPostEffect.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransitionPostEffect : MonoBehaviour
+{
+    [SerializeField] private Material postEffectMaterial; // 画面遷移ポストエフェクトのマテリアル
+    [SerializeField] private float transitionTime = 2f; // 画面遷移の時間
+    private readonly int _progressId = Shader.PropertyToID("_Progress"); // シェーダープロパティのReference名
+
+    [SerializeField] private Text resultText;
+    private bool boot = false;
+    /// <summary>
+    /// 開始時に実行
+    /// </summary>
+    void Update()
+    {
+        if (postEffectMaterial != null &&
+            !boot)
+        {
+            StartCoroutine(Transition());
+            boot = true;
+        }
+    }
+
+    /// <summary>
+    /// 画面遷移
+    /// </summary>
+    IEnumerator Transition()
+    {
+        float t = 0f;
+        while (t < transitionTime)
+        {
+            float progress = t / transitionTime;
+
+            // シェーダーの_Progressに値を設定
+            postEffectMaterial.SetFloat(_progressId, progress);
+            yield return null;
+
+            t += Time.deltaTime;
+        }
+        float a_color = 0;
+        while (a_color < 1)
+        {
+            resultText.color = new Color(resultText.color.r, resultText.color.b, resultText.color.g, a_color);
+            a_color += Time.deltaTime / 5;
+        }
+
+        postEffectMaterial.SetFloat(_progressId, 1f);
+    }
+}
